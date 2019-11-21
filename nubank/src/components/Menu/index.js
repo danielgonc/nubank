802e1c08cd1fdf807dfd19c97e175b4cb290e6f4
danielgon__ <daniel.goncalves@proaire.com.br>
2019-11-21 10:28:06 -0300

    Layout pronto

---
+++ nubank/src/components/Menu/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import QRCode from 'react-native-qrcode';
+import { Container, Code } from './styles';
+
+export default function Menu() {
+    return (
+        <Container>
+            <Code>
+                <QRCode 
+                    value="https://google.com.br"
+                    size={80}
+                    fgColor="#FFF"
+                    bgColor="#8B10AE"
+                />
+            </Code>
+
+            {/* <Nav>
+                <NavItem>
+
+                </NavItem>
+            </Nav> */}
+        </Container>
+    );
+}
